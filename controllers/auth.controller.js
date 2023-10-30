f271222928947760df520bd5e9171c5427ca786a
Shendy <Shendy1567@gmail.com>
2023-10-30 19:26:05 +0700

    adding login feature with jwt as res cookie

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,8 @@
 const { User } = require("../models");
+const jwt = require("jsonwebtoken");
 
 module.exports = class AuthControllers {
-  async reqisterPlayer(req, res, next) {
+  async reqisterUser(req, res) {
     const { username, firstName, lastName, email, pin } = req.body;
 
     if (await User.findOne({ where: { email } })) {
@@ -23,6 +24,52 @@ module.exports = class AuthControllers {
         status: "Success",
         message: "Account Successfully registered",
       });
+    } catch (error) {
+      res.status(500).json({
+        status: "Error",
+        message: error.message,
+      });
+    }
+  }
+
+  async loginUser(req, res) {
+    try {
+      const user = await User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+
+      if (!user) {
+        return res.status(400).json({
+          status: "Error",
+          message: "Email is not Found",
+        });
+      } else if (req.body.pin != user.pin) {
+        return res.status(400).json({
+          status: "Error",
+          message: "Wrong pin",
+        });
+      }
+
+      const payload = {
+        id: user.id,
+        username: user.username,
+      };
+
+      const token = jwt.sign(payload, "JwtToken");
+
+      return res
+        .cookie("token", token, {
+          maxAge: 24 * 60 * 60 * 1000,
+          path: "/",
+        })
+        .status(200)
+        .json({
+          status: "Success",
+          token: token,
+          Message: "Welcome back " + user.firstName,
+        });
     } catch (error) {
       res.status(500).json({
         status: "Error",
--- routes/api/auth.routes.js
+++ routes/api/auth.routes.js
@@ -2,6 +2,7 @@ const AuthControllers = require("../../controllers/auth.controller");
 const AuthControl = new AuthControllers();
 const AuthRouter = require("express").Router();
 
-AuthRouter.post("/register", AuthControl.reqisterPlayer);
+AuthRouter.post("/register", AuthControl.reqisterUser);
+AuthRouter.post("/login", AuthControl.loginUser);
 
 module.exports = AuthRouter;
